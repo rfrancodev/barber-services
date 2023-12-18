da6510c9671435c22b75c7b7db144a0040b47127
Rafael <francorfs@icloud.com>
2023-12-18 17:27:12 -0300

    Endpoint de criar experts

--- src/experts/experts.controller.ts
+++ src/experts/experts.controller.ts
@@ -1,7 +1,29 @@
-import { Controller } from '@nestjs/common'
+import {
+	BadGatewayException,
+	Body,
+	Controller,
+	HttpStatus,
+	Post,
+	Res
+} from '@nestjs/common'
 import { ExpertsService } from './experts.service'
+import CreateExpertsDto from './dtos/create-experts'
+import { Response } from 'express'
 
 @Controller('experts')
 export class ExpertsController {
 	constructor(private readonly expertsService: ExpertsService) {}
+
+	@Post()
+	async create(@Body() data: CreateExpertsDto, @Res() res: Response) {
+		const expertExists = await this.expertsService.findExpertByEmail(data.email)
+
+		if (expertExists) {
+			throw new BadGatewayException(
+				'Já existe um profissional com o email informado'
+			)
+		}
+		const expert = await this.expertsService.createExpert(data)
+		return res.status(HttpStatus.CREATED).json(expert)
+	}
 }
