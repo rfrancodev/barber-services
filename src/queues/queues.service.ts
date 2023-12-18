68d5b56fe1ea43f96d3841970011ab9da5e90c55
Rafael <francorfs@icloud.com>
2023-12-18 19:50:19 -0300

    Endpoint para listar as queuea

--- src/queues/queues.service.ts
+++ src/queues/queues.service.ts
@@ -19,4 +19,19 @@ export class QueuesService {
 			}
 		})
 	}
+
+	async getQueues() {
+		return await this.prisma.queue.findMany()
+	}
+
+	async getExpertQueues(expertId: string) {
+		return await this.prisma.queue.findMany({
+			where: {
+				expertId
+			},
+			include: {
+				expert: true
+			}
+		})
+	}
 }
